4f6f75d569c22ff353dbf878266bd2bf0cc685ae
David Wu <david10608@gmail.com>
2017-10-18 13:17:20 +0530

    delete unnecessary methods

--- app/src/main/java/com/github/pl4gue/mvp/view/AddHomeworkView.java
+++ app/src/main/java/com/github/pl4gue/mvp/view/AddHomeworkView.java
@@ -1,7 +1,5 @@
 package com.github.pl4gue.mvp.view;
 
-import com.github.pl4gue.data.entity.HomeWorkEntry;
-
 /**
  * @author David Wu (david10608@gmail.com)
  *         Created on 16.10.17.
@@ -12,10 +10,6 @@ public interface AddHomeworkView extends View {
 
     void hideLoadingScreen();
 
-    void updateGSheetsBy(HomeWorkEntry homeWorkEntry);
-
     void fetchDataError();
-
-    void showError();
 
 }
