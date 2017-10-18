25cc2366863e671a817ef80baf17d4bb10a481b0
David Wu <david10608@gmail.com>
2017-10-18 16:50:33 +0530

    Add Adapter for "Subject" AutoCompletionTextView

---
+++ app/src/main/java/com/github/pl4gue/adapters/AddHomework_SubjectAutoCompletionAdapter.java
@@ -0,0 +1,20 @@
+package com.github.pl4gue.adapters;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+/**
+ * @author David Wu (david10608@gmail.com)
+ *         Created on 18.10.17.
+ */
+
+public class AddHomework_SubjectAutoCompletionAdapter extends ArrayAdapter{
+
+    public AddHomework_SubjectAutoCompletionAdapter(@NonNull Context context, @LayoutRes int resource) {
+        super(context, resource);
+    }
+
+    //TODO AutoComplete Adapter
+}
